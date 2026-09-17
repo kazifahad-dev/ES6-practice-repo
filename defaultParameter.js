f78134aa48cc5956306450d355183363a6671ef9
kazi Fahad <kazifahad.net@gmail.com>
2026-09-17 22:54:11 +0600

    add first commit

---
+++ defaultParameter.js
@@ -0,0 +1,6 @@
+function restaurant  (level = "Medium"){
+    console.log("Spicy Level", level);
+    
+}
+
+restaurant("High");
---
+++ templateString.js
@@ -0,0 +1,4 @@
+let num1 = 10;
+let num2 = 20;
+
+console.log(`The total is ${num1 + num2}`);
